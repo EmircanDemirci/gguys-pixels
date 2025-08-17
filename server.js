6cc157ed5aabdaeebe23759d51a8bfd3e7fb1a98
Venoox <63316015+EmircanDemirci@users.noreply.github.com>
2025-08-17 18:08:17 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+//PORT
+const PORT = 3000;
+
+//canvas boyutu
+const WIDTH = 50;
+const HEIGHT = 50;
+
+//boş canvas
+let canvas = Array.from({ length: HEIGHT }, () => Array(WIDTH).fill("#FFFFFF"));
+
+//Static dosyaları
+app.use(express.static("public"));
+
+//Client bağlandığında
+io.on("connection", (socket) => {
+    console.log("Yeni kullanıcı bağlandı!");
+
+    //kullanıcıya mevcut canvas gösteriliyor
+    socket.emit("init", canvas);
+
+    //Kullanıcı pixel değiştirdiğinde
+    socket.on("placePixel", ({ x, y, color }) => {
+        if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+            canvas[y][x] = color;
+            // Herkese güncelleme yay
+            io.emit("updatePixel", { x, y, color });
+        }
+    });
+
+
+})
+
+server.listen(PORT, () => {
+    console.log(`Server çalışıyor: http://localhost:${PORT}`);
+});
